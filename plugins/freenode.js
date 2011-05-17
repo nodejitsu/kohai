667c1bd04d1cb5165d07ecd77516451e97d4123b
avian <avian@Sanchez.(none)>
2011-05-17 00:15:39 -0700

    [dist] Added !tweet command as per Issue #4

--- plugins/freenode.js
+++ plugins/freenode.js
@@ -8,12 +8,22 @@ module.exports = function(config) {
 				client.say("nickserv","identify "+auth.password)
                                 console.log("Connected to freenode...")
                         })
-                        this.on("message#nodetestsu", function (from, message){
+                        this.on("message" + config.channels[0], function (from, message){
                                 console.log(from, " : ", message)
                                 var talkSmack = message.match(/\bbot\b/i)
-                                var intro = message.match(/\bkohai\b/i)
-                                if(talkSmack) client.say("#nodetestsu", "'Bot' is a derogatory term, and I'm offended.")
-                                if(intro) client.say("#nodetestsu", "I am Kohai, semi-useful communications-facilitating pseudointelligence!")
+                                var intro = message.match(/^kohai\s.*/i)
+                                var tweet = message.match(/^!tweet\s/i)
+                                if(tweet) {
+                                    re = /^!tweet\s(.{1,140})/;
+                                    var tweetMatch = re.exec(message);
+                                    twit.updateStatus(tweetMatch[1], function (data) {
+                                        console.log(JSON.stringify(data))
+                                    })
+                                }
+                                if(talkSmack) client.say(config.channels[0], "'Bot' is a derogatory term, and I'm offended.")
+                                if(intro) client.say(config.channels[0], "I am Kohai, semi-useful communications-facilitating pseudointelligence!")
+                                
+                                
                         })
 		}
 	}
