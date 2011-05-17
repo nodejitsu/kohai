1bccffdd2a099294afcd6bd4dc45d64b51d0bfa2
avian <avian@Sanchez.(none)>
2011-05-17 04:35:50 -0700

    [dist] Added whitelist for trigger commands as per #6

--- plugins/alias.js
+++ plugins/alias.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
 
     var client = this
-     
+    var whitelist = config.plugins.alias.whitelist
     client.triggers = {
                             "insult" : function (message) {
                                     client.say(config.channels[0], "You," + message + ", are a quite nonsensical dallywhacker!")
@@ -10,15 +10,19 @@ module.exports = function (config) {
                             "gtfo" : function () { process.exit() }
                       }
     client.on("message",function triggerListener(name,to,message) {
-            var trigger_match = message.match(/^[!](\S+)(.*|$)?/)
-            if(trigger_match) {
-                    var trigger = trigger_match[1]
-                    
-                    var handler = client.triggers[trigger]
-                    if(handler) {
-                            var args = arghelper(trigger_match[2])
-                            handler.apply(client,[name,to,message].concat(args));
+            for (var i=0; i<whitelist.length; i++) {
+                if (whitelist[i] == name) {
+                    var trigger_match = message.match(/^[!](\S+)(.*|$)?/)
+                    if(trigger_match) {
+                            var trigger = trigger_match[1]
+                            
+                            var handler = client.triggers[trigger]
+                            if(handler) {
+                                    var args = arghelper(trigger_match[2])
+                                    handler.apply(client,[name,to,message].concat(args));
+                            }
                     }
+                }
             }
     })
  
