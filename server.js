3f8222d5c53ab74d65110181b917a516e25bc5fc
avian <avian@Sanchez.(none)>
2011-05-18 14:41:40 -0700

    [fix] Fixed indentation and readability of code

--- server.js
+++ server.js
@@ -4,39 +4,35 @@ arghelper = require("./utils/arguments.js") //this can either go into ./plugins/
 
 //our bot is exportable
 module.exports = function() {
-	//grab our configuration for the bot
-	var config = require(__dirname+"/config.js")()
+  //grab our configuration for the bot
+  var config = require(__dirname+"/config.js")()
+  //ok all of our stuff is ready to start up the bot
+  var client = new irc.Client(
+  config.server || "irc.freenode.net"
+  , config.name || "kohai"
+  , config
+  )
 
-	//ok all of our stuff is ready to start up the bot
-	var client = new irc.Client(
-		config.server || "irc.freenode.net"
-		, config.name || "kohai"
-		, config
-	)
-	
-	
-	client.on("error",function() {
-		console.log(arguments)
-	})
+  client.on("error",function() {
+    console.log(arguments)
+  })
 
-	//modules are similar to plugins
-	var modules = config.modules
+  //modules are similar to plugins
+  var modules = config.modules
+  //load all of the modules our bot uses
+  modules.forEach(function(module_name){
+    //get the modules from the modules directory relative to where *this* script lives
+    var handler = require(__dirname+"/plugins/"+module_name)
+    if(typeof handler === "function") {
+      handler.call(client, config)
+    }
 
-	//load all of the modules our bot uses
-	modules.forEach(function(module_name){
+  })
 
-		//get the modules from the modules directory relative to where *this* script lives
-		var handler = require(__dirname+"/plugins/"+module_name)
-		if(typeof handler === "function") {
-			handler.call(client, config)
-		}
-
-	})
-
-	return client;
+  return client;
 }
 
 //run this if it was the main file loaded in by node
 if(require.main) {
-	module.exports()
+  module.exports()
 }
