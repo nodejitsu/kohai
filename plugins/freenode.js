13dd3d65884388e10eeb9c948aafd0f34adc6888
avian <avian@Sanchez.(none)>
2011-05-18 14:54:24 -0700

    [fix] Fixed indentation

--- plugins/freenode.js
+++ plugins/freenode.js
@@ -1,39 +1,35 @@
 module.exports = function(config) {
-	var client = this 
-	var auth = config.auth
-	var whitelist = config.plugins.alias.whitelist
-	if(config.auth) {
-		auth = auth.freenode
-		if(auth) {
-			client.on("motd",function motd(){ 
-				client.say("nickserv","identify "+auth.password)
-                                console.log("Connected to freenode...")
-                                
-                        })
-                        config.channels.forEach(function (channel, index) {
-                            client.on("message" + channel, function (from, message){
-                                    console.log(from, " : ", message)
-                                    var talkSmack = message.match(/kohai:\s.*\bbot\b/i)
-                                    var intro = message.match(/kohai:\sintro.*/i)
-                                    var tweet = message.match(/^!tweet\s/i)
-                                    if(tweet) {
-                                        for (var i=0; i<whitelist.length; i++) {
-                                            if (whitelist[i] == from) {
-                                                re = /^!tweet\s(.{1,140})/;
-                                                var tweetMatch = re.exec(message);
-                                                twit.updateStatus(tweetMatch[1], function (data) {
-                                                    console.log("Tweeted: " + data.text + " For: " + from)
-                                                })
-                                            }
-                                        }
-                                    }
-                                    if(talkSmack) client.say(channel, "'Bot' is a derogatory term, and I'm offended.")
-                                    if(intro) client.say(channel, "I am Kohai, semi-useful communications-facilitating pseudointelligence!")
-                                    
-                                    
-                                    
-                            })
-                        })
-		}
-	}
+  var client = this 
+  var auth = config.auth
+  var whitelist = config.plugins.alias.whitelist
+  if(config.auth) {
+    auth = auth.freenode
+    if(auth) {
+      client.on("motd",function motd(){ 
+        client.say("nickserv","identify "+auth.password)
+        console.log("Connected to freenode...")
+      })
+      config.channels.forEach(function (channel, index) {
+        client.on("message" + channel, function (from, message){
+          console.log(from, " : ", message)
+          var talkSmack = message.match(/kohai:\s.*\bbot\b/i)
+          var intro = message.match(/kohai:\sintro.*/i)
+          var tweet = message.match(/^!tweet\s/i)
+          if(tweet) {
+            for (var i=0; i<whitelist.length; i++) {
+              if (whitelist[i] == from) {
+                re = /^!tweet\s(.{1,140})/;
+                var tweetMatch = re.exec(message);
+                twit.updateStatus(tweetMatch[1], function (data) {
+                  console.log("Tweeted: " + data.text + " For: " + from)
+                })
+              }
+            }
+          }
+          if(talkSmack) client.say(channel, "'Bot' is a derogatory term, and I'm offended.")
+          if(intro) client.say(channel, "I am Kohai, semi-useful communications-facilitating pseudointelligence!")
+        })
+      })
+    }
+  }
 }
